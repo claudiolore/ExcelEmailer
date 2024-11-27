775da118bc2544bb6f6c3075e3ad6a95619a6d33
claudiolore <claudio.lore001@gmail.com>
2024-11-27 11:45:54 +0100

    a

--- email-sender/Program.cs
+++ email-sender/Program.cs
@@ -20,48 +20,12 @@ namespace ExcelEmailSender
             while (true)
             {
                 ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
-                string subject = "SUPPORTO INFORMATICO PER CONTABILIZZAZIONE CALORE E MONITORAGGIO ENERGETICO";
-                string body = @"È noto che i sistemi di contabilizzazione del calore per gli impianti di riscaldamento centralizzato, resi obbligatori a partire dal 2014, 
-                attualmente devono anche:
-
-                • essere leggibili da remoto,
-                • prevedere la fornitura agli utenti finali dei dati di consumo almeno una volta al mese.
-
-                (ai sensi del D.lgs 102/2014, per maggiori dettagli si veda in allegato)
-
-                Rispondere a questi nuovi obblighi può essere oneroso. Non adeguare la contabilizzazione ai predetti requisiti può far perdere clienti.
-
-                La soluzione PLANERGY® che vi presentiamo è l'opportunità di innovare, senza investimenti, il servizio di contabilizzazione da voi 
-                erogato ai condomìni vostri clienti.
-
-                Con la nostra soluzione, la vostra azienda potrà:
-                • fidelizzare i condomìni clienti,
-                • adeguare il servizio di contabilizzazione offerto, rispondendo agli obblighi di accesso da remoto e di fornitura dati minima 
-                  (almeno una volta al mese),
-                • fornire un servizio di contabilizzazione che include un innovativo monitoraggio via web.
-
-                In allegato trova una breve descrizione dell'applicativo web PLANERGY® che la sua azienda potrà offrire ai suoi clienti.
-
-                La sua attivazione potrà avvenire con il nostro supporto informatico, basato su una Convenzione che prevede, da parte nostra:
-                • configurazione software degli apparati di raccolta e trasmissione dati,
-                • fornitura degli accessi per gli utenti finali all'applicativo web,
-                • elaborazione dati per i piani di riparto annuali (il vostro onere si ridurrà alla sola impaginazione finale personalizzata, se
-                  necessaria, ed alla consegna all'amministratore del condominio),
-                • patto di non concorrenza (ci impegniamo a non effettuare alcuna azione commerciale rivolta ai suoi clienti in concorrenza
-                  con la vostra azienda).
-
-                Se interessato, risponda per favore a questa mail, fornendo il nominativo da contattare per un incontro di approfondimento, anche
-                per verificare insieme quali apparati di raccolta e trasmissione dati (attualmente presenti ovvero che potreste proporre ai condomìni
-                interessati) siano adatti all'attivazione del nuovo servizio.
-
-                In attesa di un vostro gradito riscontro, porgiamo distinti saluti.";
-
-                //string subject = string.Empty;
-                //string body = string.Empty;
+               string subject = string.Empty;
+                string body = string.Empty;
                 string senderEmail = string.Empty;
                 string senderPassword = string.Empty;
                 string excelPath = string.Empty;
-                string outputPdfPath = "C:\\Users\\claud\\OneDrive\\Desktop\\planergy utili\\appoggio\\SUPPORTO INFORMATICO PLANERGY CONTABILIZZAZIONE.pdf";
+                string outputPdfPath = string.Empty;
                 string pdfPath = string.Empty;
                 int emailInviate = 0;
                 int emailFallite = 0;
@@ -124,44 +88,44 @@ namespace ExcelEmailSender
                 }
 
                 //INSERIMENTO OGGETTO EMAIL
-                //while (true)
-                //{
-                //    Console.ForegroundColor = ConsoleColor.Yellow;
-                //    Console.Write("\n--Inserisci l'oggetto dell'email: ");
-                //    Console.ResetColor();
-                //    subject = Console.ReadLine();
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("\n--Inserisci l'oggetto dell'email: ");
+                    Console.ResetColor();
+                    subject = Console.ReadLine();
 
-                //    if (string.IsNullOrEmpty(subject))
-                //    {
-                //        Console.ForegroundColor = ConsoleColor.Red;
-                //        Console.WriteLine("\n⚠ ATTENZIONE! Inserire almeno un carattere");
-                //        Console.WriteLine("Premi un tasto qualunque per riprovare");
-                //        Console.ResetColor();
-                //        Console.ReadKey();
-                //        continue;
-                //    }
-                //    break;
-                //}
+                    if (string.IsNullOrEmpty(subject))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\n⚠ ATTENZIONE! Inserire almeno un carattere");
+                        Console.WriteLine("Premi un tasto qualunque per riprovare");
+                        Console.ResetColor();
+                        Console.ReadKey();
+                        continue;
+                    }
+                    break;
+                }
 
-                ////INSERIMENTO CORPO EMAIL
-                //while (true)
-                //{
-                //    Console.ForegroundColor = ConsoleColor.Yellow;
-                //    Console.Write("\n--Inserisci il corpo dell'email: ");
-                //    Console.ResetColor();
-                //    body = Console.ReadLine();
+                //INSERIMENTO CORPO EMAIL
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("\n--Inserisci il corpo dell'email: ");
+                    Console.ResetColor();
+                    body = Console.ReadLine();
 
-                //    if (string.IsNullOrEmpty(body))
-                //    {
-                //        Console.ForegroundColor = ConsoleColor.Red;
-                //        Console.WriteLine("\n⚠ ATTENZIONE! Inserire almeno un carattere");
-                //        Console.WriteLine("Premi un tasto qualunque per riprovare");
-                //        Console.ResetColor();
-                //        Console.ReadKey();
-                //        continue;
-                //    }
-                //    break;
-                //}
+                    if (string.IsNullOrEmpty(body))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\n⚠ ATTENZIONE! Inserire almeno un carattere");
+                        Console.WriteLine("Premi un tasto qualunque per riprovare");
+                        Console.ResetColor();
+                        Console.ReadKey();
+                        continue;
+                    }
+                    break;
+                }
 
                 //ALLEGARE UN PDF
                 Console.ForegroundColor = ConsoleColor.Yellow;
@@ -175,7 +139,7 @@ namespace ExcelEmailSender
                     while (true)
                     {
                         Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write("\nInserisci il percorso del file PDF: ");
+                        Console.Write("\nInserisci il percorso del file PDF (deve avere una colonna nominativo, email e indirizzo: ");
                         Console.ResetColor();
                         pdfPath = Console.ReadLine().Trim('"');
 
@@ -432,7 +396,6 @@ namespace ExcelEmailSender
             try
             {
                 var message = new MimeMessage();
-                //message.From.Add(new MailboxAddress("Planergy", senderEmail));
                 message.From.Add(new MailboxAddress("", senderEmail));
                 message.To.Add(new MailboxAddress("", recipientEmail));
                 message.Subject = subject;
@@ -545,40 +508,5 @@ namespace ExcelEmailSender
             Console.ResetColor();
         }
 
-//        string subject = "SUPPORTO INFORMATICO PER CONTABILIZZAZIONE CALORE E MONITORAGGIO ENERGETICO";
-//        string body = @"È noto che i sistemi di contabilizzazione del calore per gli impianti di riscaldamento centralizzato, resi obbligatori a partire dal 2014, 
-//attualmente devono anche:
-
-//• essere leggibili da remoto,
-//• prevedere la fornitura agli utenti finali dei dati di consumo almeno una volta al mese.
-
-//(ai sensi del D.lgs 102/2014, per maggiori dettagli si veda in allegato)
-
-//Rispondere a questi nuovi obblighi può essere oneroso. Non adeguare la contabilizzazione ai predetti requisiti può far perdere clienti.
-
-//La soluzione PLANERGY® che vi presentiamo è l'opportunità di innovare, senza investimenti, il servizio di contabilizzazione da voi 
-//erogato ai condomìni vostri clienti.
-
-//Con la nostra soluzione, la vostra azienda potrà:
-//• fidelizzare i condomìni clienti,
-//• adeguare il servizio di contabilizzazione offerto, rispondendo agli obblighi di accesso da remoto e di fornitura dati minima 
-//  (almeno una volta al mese),
-//• fornire un servizio di contabilizzazione che include un innovativo monitoraggio via web.
-
-//In allegato trova una breve descrizione dell'applicativo web PLANERGY® che la sua azienda potrà offrire ai suoi clienti.
-
-//La sua attivazione potrà avvenire con il nostro supporto informatico, basato su una Convenzione che prevede, da parte nostra:
-//• configurazione software degli apparati di raccolta e trasmissione dati,
-//• fornitura degli accessi per gli utenti finali all'applicativo web,
-//• elaborazione dati per i piani di riparto annuali (il vostro onere si ridurrà alla sola impaginazione finale personalizzata, se
-//  necessaria, ed alla consegna all'amministratore del condominio),
-//• patto di non concorrenza (ci impegniamo a non effettuare alcuna azione commerciale rivolta ai suoi clienti in concorrenza
-//  con la vostra azienda).
-
-//Se interessato, risponda per favore a questa mail, fornendo il nominativo da contattare per un incontro di approfondimento, anche
-//per verificare insieme quali apparati di raccolta e trasmissione dati (attualmente presenti ovvero che potreste proporre ai condomìni
-//interessati) siano adatti all'attivazione del nuovo servizio.
-
-//In attesa di un vostro gradito riscontro, porgiamo distinti saluti.";
     }
 }
